ece686e8d5eedd2a6e68c50cac7350772ad111bf
Niharika-Razz <84461773+Niharika-Razz@users.noreply.github.com>
2023-12-29 18:48:18 +0000

    create add new button

--- src/Componants/TasksList/TasksList.jsx
+++ src/Componants/TasksList/TasksList.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { Badge } from "react-bootstrap";
+import { Badge, Button } from "react-bootstrap";
 import "./TasksList.css";
 import { STATUS_STYLES } from "../../Constants";
 
 const TasksList = ({ name }) => {
   const className = STATUS_STYLES[name];
   return (
-    <Badge pill className={className}>
-      <i class="bi bi-circle-fill" /> {name}
-    </Badge>
+    <>
+      <Badge pill className={className}>
+        <i class="bi bi-circle-fill" /> {name}
+      </Badge>
+      <div>
+        <Button size="sm" className={"add-new-button " + className}>
+          + Add New
+        </Button>
+      </div>
+    </>
   );
 };
 
