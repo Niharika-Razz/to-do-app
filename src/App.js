cf5f6b355c7941d43a1bc691f677b5a2b2e1992e
Niharika-Razz <84461773+Niharika-Razz@users.noreply.github.com>
2023-12-29 15:38:46 +0000

    create page layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import TaskBoard from "./Pages/TaskBoard/TaskBoard";
-TaskBoard;
 
 function App() {
   return (
--- src/Pages/TaskBoard/TaskBoard.jsx
+++ src/Pages/TaskBoard/TaskBoard.jsx
@@ -1,10 +1,28 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./TaskBoard.css";
 
-const TaskBoard=()=> {
+const TaskBoard = () => {
   return (
-    <div >
-      hello from task board
-    </div>
+    <Container>
+      <Row className="border-box">
+        <Col md={2} className="border-box">
+          <p className="task-board-heading">Task boards</p>
+        </Col>
+        <Col md={10} className="border-box">
+          <p className="task-board-heading">My Projects</p>
+        </Col>
+      </Row>
+      <Row className="border-box">
+        <Col md={2} className="border-box" >
+          3
+        </Col>
+        <Col  md={10} className="border-box" >
+          4
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default TaskBoard;
