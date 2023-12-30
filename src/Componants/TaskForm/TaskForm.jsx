99d4263211d8fa6c1f92c2142538722fe758d2e1
Niharika-Razz <84461773+Niharika-Razz@users.noreply.github.com>
2023-12-30 09:20:40 +0000

    change layout of the form properly

--- src/Componants/TaskForm/TaskForm.jsx
+++ src/Componants/TaskForm/TaskForm.jsx
@@ -5,41 +5,34 @@ import { TASK_STATUS } from "../../Constants";
 const TaskForm = () => {
   return (
     <Container>
-      <Form onSubmit={() => {}}>
-        <Form.Group as={Row} className="mb-3">
-          <Form.Label column sm={2}>
-            Name of the Task
-          </Form.Label>
-          <Col sm={10}>
-            <Form.Control type="text" name="taskName" />
-          </Col>
-        </Form.Group>
-        <Form.Group as={Row} className="mb-3">
-          <Form.Label column sm={2}>
-            Start Date
-          </Form.Label>
-          <Col sm={4}>
-            <Form.Control type="date" name="startDate" />
-          </Col>
-          <Form.Label column sm={2}>
-            Deadline
-          </Form.Label>
-          <Col sm={4}>
-            <Form.Control type="date" name="deadLine" />
-          </Col>
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label>Name of the Task</Form.Label>
+          <Form.Control type="text" name="taskName" placeholder="text" />
         </Form.Group>
 
-        <Form.Group as={Row} className="mb-3">
+        <Form.Group className="mb-3">
+          <Row>
+            <Col>
+              <Form.Label>Start Date</Form.Label>
+              <Form.Control type="date" name="startDate" />
+            </Col>
+            <Col>
+              <Form.Label>Deadline</Form.Label>
+              <Form.Control type="date" name="deadLine" />
+            </Col>
+          </Row>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
           <Form.Label column sm={2}>
             Status
           </Form.Label>
-          <Col sm={10}>
-            <Form.Select name="status">
-              {Object.values(TASK_STATUS).map((status) => (
-                <option value={status}>{status}</option>
-              ))}
-            </Form.Select>
-          </Col>
+          <Form.Select name="status">
+            {Object.values(TASK_STATUS).map((status) => (
+              <option value={status}>{status}</option>
+            ))}
+          </Form.Select>
         </Form.Group>
       </Form>
     </Container>
