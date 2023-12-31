41e5d98f6a94db1a14081f2d0a4a66bab2c5b14e
Niharika-Razz <84461773+Niharika-Razz@users.noreply.github.com>
2023-12-31 07:29:33 +0000

    add validations to the form

--- src/Componants/TaskForm/TaskForm.jsx
+++ src/Componants/TaskForm/TaskForm.jsx
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Col, Row, Container, FormGroup, Button } from "react-bootstrap";
 import { TASK_STATUS } from "../../Constants";
 
 const TaskForm = ({ onCancel, onSubmit }) => {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === true) {
+      setValidated(true);
+      onSubmit();
+    }
+  };
+
   return (
     <Container>
-      <Form>
+      <Form noValidate validated={validated}>
         <Form.Group className="mb-3" controlId="taskName">
           <Form.Label>Name of the Task</Form.Label>
-          <Form.Control size="sm" type="text" placeholder="text" />
+          <Form.Control size="sm" type="text" placeholder="text" required />
+          <Form.Control.Feedback type="invalid">
+            Please fill the task name
+          </Form.Control.Feedback>
         </Form.Group>
         <Row>
           <Col>
             <Form.Group className="mb-3" controlId="startDate">
               <Form.Label>Start Date</Form.Label>
-              <Form.Control size="sm" type="date" />
+              <Form.Control size="sm" type="date" required />
+              <Form.Control.Feedback type="invalid">
+                Please fill the start date
+              </Form.Control.Feedback>
             </Form.Group>
           </Col>
           <Col>
             <FormGroup className="mb-3" controlId="deadLine">
               <Form.Label>Deadline</Form.Label>
-              <Form.Control size="sm" type="date" />
+              <Form.Control size="sm" type="date" required />
+              <Form.Control.Feedback type="invalid">
+                Please fill the deadline date
+              </Form.Control.Feedback>
             </FormGroup>
           </Col>
         </Row>
@@ -40,7 +59,7 @@ const TaskForm = ({ onCancel, onSubmit }) => {
           </Button>
           <Button
             variant="primary"
-            onClick={onSubmit}
+            onClick={handleSubmit}
             size="sm"
             style={{ marginLeft: "1em" }}
           >
